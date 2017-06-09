e0370b34ad8549a88a6e0b18f94bd69be37023d5
JBayona <jordavids_22@hotmail.com>
2017-06-08 22:33:16 -0500

    uniqueCharacters explanation

--- Arrays and Strings/uniqueCharacters.js
+++ Arrays and Strings/uniqueCharacters.js
@@ -4,13 +4,16 @@ if you cannot use additional data structures?
 */
 
 function isUniqueChars(str){
+  //Creamos un array con una posición por letra y llenamos todo el false
   var alphabetArray = new Array(26).fill(false);
   var index = 0;
   for(var i = 0; i < str.length; i++){
+    //a = 0, b = 1, c = 2,...
     index = str[i].charCodeAt(0) - 'a'.charCodeAt(0);
+	//Si no hemos encontrado el elemento, ponemos en true ya que lo hemos analizado
     if(!alphabetArray[index]){
       alphabetArray[index] = true;
-    }else{
+    }else{ //Si ya estaba en true significa que ya lo encontramos y retornamos false ya que hay repetido
       return false;
     }
   }
